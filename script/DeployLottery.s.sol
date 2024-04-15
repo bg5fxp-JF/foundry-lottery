dafc096e39a40800ea3ffc833669c26e1471b383
bg5fxp-JF <jacobfecunda@outlook.com>
2024-04-15 13:08:28 +0100

    added dynamic deployment scripts

---
+++ script/DeployLottery.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {Lottery} from "../src/Lottery.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployLottery is Script {
+    function run() public returns (Lottery) {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint64 subscriptionId,
+            bytes32 gasLane,
+            uint256 interval,
+            uint256 entranceFee,
+            uint32 callbackGasLimit,
+            address vrfCoordinatorV2
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+
+        Lottery lottery =
+            new Lottery(subscriptionId, gasLane, interval, entranceFee, callbackGasLimit, vrfCoordinatorV2);
+
+        vm.stopBroadcast();
+
+        return lottery;
+    }
+}
